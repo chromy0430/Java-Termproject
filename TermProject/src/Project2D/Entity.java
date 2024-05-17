7628968f4621467032ebbafe8974a09a7a345442
jyouni2001 <jyouni2001@gmail.com>
2024-05-17 21:28:45 +0900

    일단 하는중

--- TermProject/src/Project2D/Entity.java
+++ TermProject/src/Project2D/Entity.java
@@ -7,6 +7,12 @@ public class Entity
 	public int x, y;
 	public int speed;
 	
-	public BufferedImage up1, up2, down1, down2, left2, right1, right2;
+	public BufferedImage up1, up2, up3, up4, up5, up6, up7;
+	public BufferedImage down1, down2, down3, down4, down5, down6, down7;
+	public BufferedImage left1, left2, left3, left4, left5, left6, left7;
+	public BufferedImage right1, right2,right3, right4, right5, right6, right7;
 	public String direction;
+	
+	public int spriteCounter = 0;
+	public int spriteNum = 1;
 }
--- TermProject/src/Project2D/Player.java
+++ TermProject/src/Project2D/Player.java
@@ -6,6 +6,7 @@ import java.awt.Graphics2D;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import java.awt.image.*;
 
 public class Player extends Entity {
 
@@ -18,6 +19,7 @@ public class Player extends Entity {
 		this.keyH = keyH;
 
 		setDefaultValues();
+		getPlayerImage();
 	}
 
 	public void setDefaultValues() {
@@ -25,12 +27,47 @@ public class Player extends Entity {
 		x = 100;
 		y = 100;
 		speed = 4;
+		direction = "down";
 	}
 
 	public void getPlayerImage() {
 		try {
 
-			up1 = ImageIO.read(getClass().getResourceAsStream("/player/golem-walk.png"));
+			// 위로 걷기 사진
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/w1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/w2.png"));
+			up3 = ImageIO.read(getClass().getResourceAsStream("/player/w3.png"));
+			up4 = ImageIO.read(getClass().getResourceAsStream("/player/w4.png"));
+			up5 = ImageIO.read(getClass().getResourceAsStream("/player/w5.png"));
+			up6 = ImageIO.read(getClass().getResourceAsStream("/player/w6.png"));
+			up7 = ImageIO.read(getClass().getResourceAsStream("/player/w7.png"));
+
+			// 왼쪽으로 걷기 사진
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/a1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/a2.png"));
+			left3 = ImageIO.read(getClass().getResourceAsStream("/player/a3.png"));
+			left4 = ImageIO.read(getClass().getResourceAsStream("/player/a4.png"));
+			left5 = ImageIO.read(getClass().getResourceAsStream("/player/a5.png"));
+			left6 = ImageIO.read(getClass().getResourceAsStream("/player/a6.png"));
+			left7 = ImageIO.read(getClass().getResourceAsStream("/player/a7.png"));
+
+			// 아래로 걷기 사진
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/s1.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/s2.png"));
+			down3 = ImageIO.read(getClass().getResourceAsStream("/player/s3.png"));
+			down4 = ImageIO.read(getClass().getResourceAsStream("/player/s4.png"));
+			down5 = ImageIO.read(getClass().getResourceAsStream("/player/s5.png"));
+			down6 = ImageIO.read(getClass().getResourceAsStream("/player/s6.png"));
+			down7 = ImageIO.read(getClass().getResourceAsStream("/player/s7.png"));
+
+			// 오른쪽으로 걷기 사진
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/d1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/d2.png"));
+			right3 = ImageIO.read(getClass().getResourceAsStream("/player/d3.png"));
+			right4 = ImageIO.read(getClass().getResourceAsStream("/player/d4.png"));
+			right5 = ImageIO.read(getClass().getResourceAsStream("/player/d5.png"));
+			right6 = ImageIO.read(getClass().getResourceAsStream("/player/d6.png"));
+			right7 = ImageIO.read(getClass().getResourceAsStream("/player/d7.png"));
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -43,41 +80,173 @@ public class Player extends Entity {
 		// 각 방향키가 눌렸을 때 플레이어 위치 업데이트
 //		if (keyH.upPressed == true) // W키 눌리면
 //		{
+//			direction = "up";
 //			y -= speed;
 //		} else if (keyH.downPressed == true) // S키 눌리면
 //		{
+//			direction = "down";
 //			y += speed;
 //		} else if (keyH.leftPressed == true) // A키 눌리면
 //		{
+//			direction = "left";
 //			x -= speed;
 //		} else if (keyH.rightPressed == true) // D키 눌리면
 //		{
+//			direction = "right";
 //			x += speed;
 //		}
 		// 대각선 구현완료
+		if (keyH.upPressed == true || keyH.downPressed || keyH.leftPressed || keyH.leftPressed) {
+			
+		}
+		
 		if (keyH.upPressed == true) // W키 눌리면
 		{
+			direction = "up";
 			y -= speed;
 		}
 		if (keyH.downPressed == true) // S키 눌리면
 		{
+			direction = "down";
 			y += speed;
 		}
 		if (keyH.leftPressed == true) // A키 눌리면
 		{
+			direction = "left";
 			x -= speed;
 		}
 		if (keyH.rightPressed == true) // D키 눌리면
 		{
+			direction = "right";
 			x += speed;
+		}
+
+		spriteCounter++;
+		if (spriteCounter > 10) {
+			if (spriteNum == 1) {
+				spriteNum = 2;
+			} else if (spriteNum == 2) {
+				spriteNum = 3;
+			} else if (spriteNum == 3) {
+				spriteNum = 4;
+			} else if (spriteNum == 4) {
+				spriteNum = 5;
+			} else if (spriteNum == 5) {
+				spriteNum = 6;
+			} else if (spriteNum == 6) {
+				spriteNum = 7;
+			} else if (spriteNum == 7) {
+				spriteNum = 1;
+			} else if (spriteNum == 2) {
+				spriteNum = 1;
+			}
+			spriteCounter = 0;
 		}
 	}
 
 	public void draw(Graphics2D g2) {
 
-		g2.setColor(Color.white);
-		// g2.fillRoundRect(playerX, playerY, 48, 48, 48, 48);
-		g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+//		g2.setColor(Color.white);
+//		g2.fillRoundRect(playerX, playerY, 48, 48, 48, 48);
+//		g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+		BufferedImage image = null;
+
+		switch (direction) {
+		case "up":
+			if (spriteNum == 1) {
+				image = up1;
+			}
+			if (spriteNum == 2) {
+				image = up2;
+			}
+			if (spriteNum == 3) {
+				image = up3;
+			}
+			if (spriteNum == 4) {
+				image = up4;
+			}
+			if (spriteNum == 5) {
+				image = up5;
+			}
+			if (spriteNum == 6) {
+				image = up6;
+			}
+			if (spriteNum == 7) {
+				image = up7;
+			}
+			break;
+		case "down":
+			if (spriteNum == 1) {
+				image = down1;
+			}
+			if (spriteNum == 2) {
+				image = down2;
+			}
+			if (spriteNum == 3) {
+				image = down3;
+			}
+			if (spriteNum == 4) {
+				image = down4;
+			}
+			if (spriteNum == 5) {
+				image = down5;
+			}
+			if (spriteNum == 6) {
+				image = down6;
+			}
+			if (spriteNum == 7) {
+				image = down7;
+			}
+			break;
+		case "left":
+			if (spriteNum == 1) {
+				image = left1;
+			}
+			if (spriteNum == 2) {
+				image = left2;
+			}
+			if (spriteNum == 3) {
+				image = left3;
+			}
+			if (spriteNum == 4) {
+				image = left4;
+			}
+			if (spriteNum == 5) {
+				image = left5;
+			}
+			if (spriteNum == 6) {
+				image = left6;
+			}
+			if (spriteNum == 7) {
+				image = left7;
+			}
+			break;
+		case "right":
+			if (spriteNum == 1) {
+				image = right1;
+			}
+			if (spriteNum == 2) {
+				image = right2;
+			}
+			if (spriteNum == 3) {
+				image = right3;
+			}
+			if (spriteNum == 4) {
+				image = right4;
+			}
+			if (spriteNum == 5) {
+				image = right5;
+			}
+			if (spriteNum == 6) {
+				image = right6;
+			}
+			if (spriteNum == 7) {
+				image = right7;
+			}
+			break;
+		}
+		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 
 	}
 }
